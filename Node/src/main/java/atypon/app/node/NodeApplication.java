13241138b41558d133e301e26839f7271d75705d
Ali Mohammad <Aliwasfi12321@gmail.com>
2023-09-04 20:06:47 +0300

    Finished setting up Eureka Server and ran it using docker-compose along with the nodes

--- Node/src/main/java/atypon/app/node/NodeApplication.java
+++ Node/src/main/java/atypon/app/node/NodeApplication.java
@@ -5,10 +5,10 @@ import atypon.app.node.model.Node;
 import atypon.app.node.utility.FileOperations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NodeApplication {
     public static void main(String[] args) {
         SpringApplication.run(NodeApplication.class, args);
